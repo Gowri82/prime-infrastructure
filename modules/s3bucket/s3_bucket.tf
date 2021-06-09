5446d83304bb491ab1d2cfe142b262c3dad02567
Jai Praveen David <37609823+jaipraveen@users.noreply.github.com>
2021-06-09 12:40:02 +0100

    Update s3_bucket.tf

--- modules/s3bucket/s3_bucket.tf
+++ modules/s3bucket/s3_bucket.tf
@@ -2,10 +2,10 @@
 
 
 resource "aws_s3_bucket" "data" {
-  bucket = "${var.project}-${var.user-role}-${var.region}-data"
+  bucket = "${var.project}-${var.user-role}-${var.region}-${var.user-role}-data"
   force_destroy = true  
 
   tags = {
-    Name        = "${var.project}-${var.user-role}-s3"
+    Name        = "${var.project}-${var.user-role}-${var.region}-${var.user-role}-s3"
   }
 }
